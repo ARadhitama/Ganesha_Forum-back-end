f4477386eb9fd2ad7795fb5a858fef247542a3f2
Dwi Cahyo Pangestu <dcpangestu@mail.com>
2019-11-16 13:28:38 +0700

    membuat fungsi untuk post

---
+++ Route/post.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('../Datastore/config');
+
+function getUser() {
+    return new Promise((resolve, reject) => {
+        db.any('SELECT user_id, email, status_admin, date_created, status_ban FROM user')
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+function getPost() {
+    return new Promise((resolve, reject) => {
+        db.any('SELECT * FROM post')
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+function getPostByID(id) {
+    return new Promise((resolve, reject) => {
+        db.any('SELECT * FROM post WHERE id = ' + id)
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+function makePost(payload){
+    return new Promise((resolve, reject) => {
+        const info = [
+            payload.topik,
+            payload.id_user,
+            payload.title,
+            payload.text,
+            today = new Date()
+        ]
+
+        db.any('INSERT INTO user(topik, id_user, title, text, date) VALUES ($1, $2, $3, $4, $5) RETURNING *', info)
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+function deletePost(id_post, id_user) {
+    return new Promise((resolve, reject) => {
+        db.any(`DELETE FROM post WHERE id = ${id_post} AND id_user = ${id_user}`)
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+function addLikes(id_post, id_user) {
+    return new Promise((resolve, reject) => {
+        db.any(`UPDATE user SET like = like + 1 WHERE id = ${id_post} AND id_user = ${id_user} RETURNING *`)
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+function reportPost(id_post, id_user) {
+    return new Promise((resolve, reject) => {
+        db.any(`UPDATE post SET report = true WHERE id = ${id_post} AND id_user = ${id_user}`)
+            .then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
