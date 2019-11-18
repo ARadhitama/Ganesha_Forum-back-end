2e8232b52f8ebc7fb66fade72b0f7f5715e8e67f
Dwi Cahyo Pangestu <dcpangestu@mail.com>
2019-11-18 18:50:44 +0700

    implementasi endpoints post.js

--- Route/post.js
+++ Route/post.js
@@ -3,18 +3,6 @@ const router = express.Router();
 const path = require('path');
 const db = require('../Datastore/config');
 
-function getUser() {
-    return new Promise((resolve, reject) => {
-        db.any('SELECT user_id, email, status_admin, date_created, status_ban FROM user')
-            .then(data => {
-                resolve(data);
-            })
-            .catch(err => {
-                reject(err);
-            })
-    })
-}
-
 function getPost() {
     return new Promise((resolve, reject) => {
         db.any('SELECT * FROM post')
@@ -59,7 +47,7 @@ function makePost(payload){
     })
 }
 
-function deletePost(id_post, id_user) {
+function deletePost(id_post) {
     return new Promise((resolve, reject) => {
         db.any(`DELETE FROM post WHERE id = ${id_post} AND id_user = ${id_user}`)
             .then(data => {
@@ -71,7 +59,7 @@ function deletePost(id_post, id_user) {
     })
 }
 
-function addLikes(id_post, id_user) {
+function addLikes(id_post) {
     return new Promise((resolve, reject) => {
         db.any(`UPDATE user SET like = like + 1 WHERE id = ${id_post} AND id_user = ${id_user} RETURNING *`)
             .then(data => {
@@ -83,7 +71,7 @@ function addLikes(id_post, id_user) {
     })
 }
 
-function reportPost(id_post, id_user) {
+function reportPost(id_post) {
     return new Promise((resolve, reject) => {
         db.any(`UPDATE post SET report = true WHERE id = ${id_post} AND id_user = ${id_user}`)
             .then(data => {
@@ -94,3 +82,62 @@ function reportPost(id_post, id_user) {
             })
     })
 }
+
+router.get('/', async function(req,res) {
+    try {
+        const admin = await getPost();
+        res.send(admin);
+    }catch(err) {
+        console.log(err);
+        res.send(err);
+    }
+})
+
+router.post('/', async function(req,res) {
+    try {
+        const admin = await makePost(req.body);
+        res.send(admin);
+    }catch(err) {
+        console.log(err);
+        res.send(err);
+    }
+})
+router.get('/:id', async function(req,res) {
+    try {
+        const admin = await getPostByID(req.params.id);
+        res.send(admin);
+    }catch(err) {
+        console.log(err);
+        res.send(err);
+    }
+})
+
+router.delete('/:id', async function(req,res) {
+    try {
+        const admin = await deletePost(req.params.id);
+        res.send(admin);
+    }catch(err) {
+        console.log(err);
+        res.send(err);
+    }
+})
+
+router.post('like/:id', async function(req,res) {
+    try {
+        const admin = await addLikes(req.params.id);
+        res.send(admin);
+    }catch(err) {
+        console.log(err);
+        res.send(err);
+    }
+})
+
+router.get('/report/:id', async function(req,res) {
+    try {
+        const admin = await reportPost(req.params.id);
+        res.send(admin);
+    }catch(err) {
+        console.log(err);
+        res.send(err);
+    }
+})
